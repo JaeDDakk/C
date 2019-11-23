15cab616e182e5c18b67e9f88129677f9cc6f815
JaeDDakk <jaegchief1@naver.com>
2019-11-24 01:19:33 +0900

    커밋

--- 2/배열/Project10/소스10.c
+++ 2/배열/Project10/소스10.c
@@ -6,7 +6,23 @@
 #include<stdio.h>
 int main()
 {
-	char a;
-	char asdf[26];
+	char ch;
+	char asdf[26] = { 0 };
 	int b = 65;
+
+	while (1)
+	{
+		printf("ют╥б >> ");
+		scanf(" %c", &ch);
+		if (ch >= 'A' || ch <= 'Z')
+		{
+			asdf[ch - 'A'] = ch;
+		}
+		else
+			break;
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		printf("%c:",)
+	}
 }
