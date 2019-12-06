252b4825f8e9f91566d87a9022a755d0ad1d440b
이 재국 <jaegchief1@naver.com>
2019-12-07 02:01:30 +0900

    모두 커밋

--- 2/배열/Project11/소스11.c
+++ 2/배열/Project11/소스11.c
@@ -6,7 +6,24 @@
  *(입력이0인숫자는출력하지않음)
  */
 #include <stdio.h>
+#define size 10
 int main()
 {
-	
+	int arr[size] = { 0 };
+	int i, count = 0, a;
+
+	do
+	{
+		printf("입력 >> ");
+		scanf("%d", &a);
+		if (a == 0)
+			break;
+		arr[a / 10]++;
+	} while (a >= 0 && a < 100);
+	for (i = 0; i < size; i++)
+	{
+		if (arr[i] != 0)
+			printf("  %d : %d\n", i, arr[i]);
+	}
+	return 0;
 }
--- 3/포인터 문제/Project11/소스11.c
+++ 3/포인터 문제/Project11/소스11.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
+#define size 10
 int main()
+{
+	int arr[size] = { 0 };
+	int i, a;
+	int* p = arr;
+	do
+	{
+		printf("ют╥б >> ");
+		scanf("%d", &a);
+		if (a == 0)
+			break;
+		*(p + (a / 10)) += 1;	//arr[a / 10]++;
+	}while (a >= 0 && a < 100);
+	for (i = 0; i < size; i++)
+	{
+		if (*(p + i) != 0)
+			printf("  %d : %d\n", i, *(p + i));
+	}
+	return 0;
+}
