9f4fe2f8b366e4b3a30c603a916d45cf090d2d6f
이 재국 <jaegchief1@naver.com>
2019-12-07 18:52:20 +0900

    모두 커밋

--- 3/포인터 문제/Project11/소스11.c
+++ 3/포인터 문제/Project11/소스11.c
@@ -11,12 +11,14 @@ int main()
 		scanf("%d", &a);
 		if (a == 0)
 			break;
-		*(p + (a / 10)) += 1;	//arr[a / 10]++;
+		*(p + (a / 10)) += 1;	
+		//arr[a / 10]++;
 	}while (a >= 0 && a < 100);
 	for (i = 0; i < size; i++)
 	{
 		if (*(p + i) != 0)
 			printf("  %d : %d\n", i, *(p + i));
+									//arr[i]
 	}
 	return 0;
 }
--- 3/포인터 문제/Project12/소스12.c
+++ 3/포인터 문제/Project12/소스12.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+#define rows 2
+#define cols 4
+int main()
+{
+	int asdf_1[rows][cols];
+	int asdf_2[rows][cols];
+	int mul;
+	int* p1[] = asdf_1[cols];
+	int* p2[] = asdf_2[cols];
+
+	printf("first array\n");
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+			scanf("%d", asdf_1[j] + i); 
+		printf("\n");
+	}
+	printf("second array\n");
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+			scanf("%d", asdf_2[j] + i);
+		printf("\n");
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			mul = *(asdf_1[j] + i) * *(asdf_2[j] + i);
+			printf("%3d ", mul);
+		}printf("\n");
+	}return 0;
+}
