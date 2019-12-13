cfacf5da9a8c154078448d87f94d3d1eccb40c46
이 재국 <jaegchief1@naver.com>
2019-12-13 20:58:58 +0900

    aa

--- C_express_pratice/포인터/포인터/Project06/소스06.c
+++ C_express_pratice/포인터/포인터/Project06/소스06.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#define SIZE 10
 void array_copy(int* a, int* b, int size)
 {
 	int i;
 	for (i = 0; i < size; i++)
 	{
-
+		*(b + i) = *(a + i);
 	}
 }
 int main()
 {
-	
+	int a[SIZE] = {1,2,3,0,};
+	int b[SIZE];
+
+	array_copy(a,b,SIZE);
 }
